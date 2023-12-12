bec84dd2735ac9e8c6c6f48e581e6ca443d7206a
siwam <siwamsingh2003@gmail.com>
2023-12-12 15:46:33 +0530

    bug: fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
               ChartGen
             </span>
           </Link>
-          <div className="flex items-center lg:order-2">
+          <div className="hidden md:flex items-center lg:order-2">
             <Link
               to="#"
               className="text-gray-400 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
